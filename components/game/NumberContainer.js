353b99773679c8ac22977fcba96b50b45fbc6313
Borys <dev.bosk@gmail.com>
2024-09-10 00:10:18 +0200

    added icons and fonts

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -22,8 +22,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   numberText: {
+    font: "open-sans-bold",
     color: Colors.accent500,
     fontSize: 36,
-    fontWeight: "bold",
   },
 });
